f7056338681849b01d12b9a676a514b0c94ce02b
Dhanesh Mane <crazyobject@gmail.com>
2025-05-01 15:34:21 +0530

    UI improvements

---
+++ src/service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing...");
+  event.waitUntil(
+    caches.open("static-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/src/main.tsx",
+        "/src/index.css",
+        "/manifest.json",
+        "/icons/icon-192x192.png",
+        "/icons/icon-512x512.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
